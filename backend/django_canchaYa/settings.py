3c953021465a8909eb79c629b1d19761d44f9062
Santiago Camargo <santiagocamargoardila@gmail.com>
2025-11-19 11:32:49 -0500

    Update settings.py

--- backend/django_canchaYa/settings.py
+++ backend/django_canchaYa/settings.py
@@ -167,6 +167,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -209,3 +211,4 @@ CORS_ALLOW_CREDENTIALS = True
 
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.User'
+
